8b0b9069bb64707c6017ae63b3fd9ee027dcd137
Klaus Aschenbrenner <klaus.aschenbrenner@sqlpassion.at>
2022-06-30 22:28:12 +0200

    Get current date and time from BIOS.

--- main64/kaosldr_x16/entry.asm
+++ main64/kaosldr_x16/entry.asm
@@ -2,29 +2,84 @@
 [BITS 16]
 
 MAIN:
-    ; Get the current date from the BIOS
-    MOV     AH, 0x4
-    INT     0x1A
-    PUSH    DX
-    XOR     AH, AH
-    MOV     AL, CH
-    CALL    BCD2DEC
-    MOV     BX, 100
-    MUL     BX
-    XOR     CH, CH
-    XCHG    CX, AX
-    CALL    BCD2DEC
-    XCHG    CX, AX
-    XOR     CH, CH
-    ADD     AX, CX
-
     ; Print out a welcome message
-    MOV     SI, WelcomeMessage
-    CALL    PrintLine
+    MOV     SI, BootMessage
+    CALL    PrintString
 
-    JMP     $
+    ; Get the current date from the BIOS
+    CALL    GetDate
+
+    ; Get the current time from the BIOS
+    CALL    GetTime
+
+    ; Print out the current date - year part
+    MOV     SI, YearString
+    CALL    PrintString
+    MOV     AX, [Year]
+    CALL    PrintDecimal
+    MOV     SI, CRLF
+    CALL    PrintString
+
+    ; Print out the current date - month part
+    MOV     SI, MonthString
+    CALL    PrintString
+    MOV     AX, [Month]
+    CALL    PrintDecimal
+    MOV     SI, CRLF
+    CALL    PrintString
+
+    ; Print out the current date - day part
+    MOV     SI, DayString
+    CALL    PrintString
+    MOV     AX, [Day]
+    CALL    PrintDecimal
+    MOV     SI, CRLF
+    CALL    PrintString
+
+    ; Print out the current time - hour part
+    MOV     SI, HourString
+    CALL    PrintString
+    MOV     AX, [Hour]
+    CALL    PrintDecimal
+    MOV     SI, CRLF
+    CALL    PrintString
+
+    ; Print out the current time - minute part
+    MOV     SI, MinuteString
+    CALL    PrintString
+    MOV     AX, [Minute]
+    CALL    PrintDecimal
+    MOV     SI, CRLF
+    CALL    PrintString
+
+    ; Print out the current time - second part
+    MOV     SI, SecondString
+    CALL    PrintString
+    MOV     AX, [Second]
+    CALL    PrintDecimal
+    MOV     SI, CRLF
+    CALL    PrintString
+
+    RET
 
 ; Include some helper functions
 %INCLUDE "functions.asm"                                                                                                                                        
 
-WelcomeMessage: DB 'Executing KAOSLDR_x16.bin...', 0xD, 0xA, 0x0
+BootMessage:    DB 'Executing KAOSLDR_x16.bin...', 0xD, 0xA, 0x0
+YearString:     DB 'Year: ', 0x0
+MonthString:    DB 'Month: ', 0x0
+DayString:      DB 'Day: ', 0x0
+HourString:     DB 'Hour: ', 0x0
+MinuteString:   DB 'Minute: ', 0x0
+SecondString:   DB 'Second: ', 0x0
+CRLF:           DB 0xD, 0xA, 0x0
+
+Year1           DW 0x00
+Year2           DW 0x00
+Year            DW 0x00
+Month           DW 0x00
+Day             DW 0x00
+
+Hour            DW 0x00
+Minute          DW 0x00
+Second          DW 0x00
--- main64/kaosldr_x16/functions.asm
+++ main64/kaosldr_x16/functions.asm
@@ -6,7 +6,7 @@
 ; This function prints a whole string, where the 
 ; input string is stored in the register "SI"
 ;================================================
-PrintLine:
+PrintString:
     ; Set the TTY mode
     MOV     AH, 0xE
     INT     10
@@ -14,20 +14,122 @@ PrintLine:
     ; Set the input string
     MOV     AL, [SI]
     CMP     AL, 0
-    JE      PrintLine_End
+    JE      PrintString_End
     
     INT     0x10
     INC     SI
-    JMP     PrintLine
+    JMP     PrintString
     
-    PrintLine_End:
+    PrintString_End:
 RET
 
-BCD2DEC:
+;================================================
+; This function prints out a decimal number
+; that is stored in the register AX.
+;================================================
+PrintDecimal:
+    MOV     CX, 0
+    MOV     DX, 0
+
+PrintDecimal_Start:
+    CMP     AX ,0
+    JE      PrintDecimal_Print
+    MOV     BX, 10
+    DIV     BX
+    PUSH    DX
+    INC     CX
+    XOR     DX, DX
+    JMP     PrintDecimal_Start
+PrintDecimal_Print:
+    CMP     CX, 0
+    JE      PrintDecimal_Exit
+    POP     DX
+        
+    ; Add 48 so that it represents the ASCII value of digits
+    MOV     AL, DL
+    ADD     AL, 48
+    MOV     AH, 0xE
+    INT     0x10
+
+    DEC     CX
+    JMP     PrintDecimal_Print
+PrintDecimal_Exit:
+RET
+
+;================================================
+; This function converts a BCD number to a
+; decimal number.
+;================================================
+Bcd2Decimal:
     MOV     CL, AL
     SHR     AL, 4
     MOV     CH, 10
     MUL     CH
     AND     CL, 0Fh
     ADD     AL, CL
+RET
+
+;=================================================
+; This function retrieves the date from the BIOS.
+;=================================================
+GetDate:
+    ; Get the current date from the BIOS
+    MOV     AH, 0x4
+    INT     0x1A
+
+    ; Century
+    PUSH    CX
+    MOV     AL, CH
+    CALL    Bcd2Decimal
+    MOV     [Year1], AX
+    POP     CX
+
+    ; Year
+    MOV     AL, CL
+    CALL    Bcd2Decimal
+    MOV     [Year2], AX
+
+    ; Month
+    MOV     AL, DH
+    CALL    Bcd2Decimal 
+    MOV     [Month], AX
+
+    ; Day
+    MOV     AL, DL
+    CALL    Bcd2Decimal
+    MOV     [Day], AX
+
+    ; Calculate the whole year (e.g. "20" * 100 + "22" = 2022)
+    MOV     AX, [Year1]
+    MOV     BX, 100
+    MUL     BX
+    MOV     BX, [Year2]
+    ADD     AX, BX
+    MOV     [Year], AX
+RET
+
+;=================================================
+; This function retrieves the time from the BIOS.
+;=================================================
+GetTime:
+    ; Get the current time from the BIOS
+    MOV     AH, 0x2
+    INT     0x1A
+
+    ; Hour
+    PUSH    CX
+    MOV     AL, CH
+    CALL    Bcd2Decimal
+    MOV     [Hour], AX
+    POP     CX
+
+    ; Minute
+    MOV     AL, CL
+    CALL    Bcd2Decimal
+    MOV     [Minute], AX
+
+    ; Second
+    MOV     AL, DH
+    CALL    Bcd2Decimal
+    MOV     [Second], AX
 RET
