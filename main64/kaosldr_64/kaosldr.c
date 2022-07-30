8dba4ae2689dcc81b1e1e800193f8b228625a87d
Klaus Aschenbrenner <klaus.aschenbrenner@sqlpassion.at>
2022-07-30 16:32:32 +0200

    Fixed a bug during the Kernel execution.

--- main64/kaosldr_64/kaosldr.c
+++ main64/kaosldr_64/kaosldr.c
@@ -1,15 +1,15 @@
 #include "fat12.h"
 
-// Implemented in assembly, jumps to 0x100000 where the Kernel was loaded
+// Implemented in assembly, continues at memory location 0x100000 where the Kernel was loaded
 extern void ExecuteKernel();
 
 // Entry point of KLDR64.BIN
-// The only task of the KLDR64.BIN file is to load the KERNEL.BIN file to the physical 
+// The only purpose of the KLDR64.BIN file is to load the KERNEL.BIN file to the physical 
 // memory address 0x100000 and execute it from there.
 //
-// That task must be done in KLDR64.BIN, because the CPU is now already in x64 Long Mode,
+// This task must be done in KLDR64.BIN, because the CPU is now already in x64 Long Mode,
 // and therefore we can access higher memory addresses like 0x100000.
-// This would be not possible in KLDR16.BIN, because the CPU is here still in x16 Real Mode.
+// This would be impossible to do in KLDR16.BIN, because the CPU is at that point in time still in x16 Real Mode.
 void kaosldr_main()
 {
     // Load the x64 OS Kernel into memory for its execution...
--- main64/kaosldr_64/kernel.asm
+++ main64/kaosldr_64/kernel.asm
@@ -3,6 +3,6 @@
 
 ; Executes the loaded x64 OS Kernel
 ExecuteKernel:
-    ; Jump to the memory location where the Kernel was loaded...
-    MOV RAX, QWORD 0x100000
-    JMP RAX
+    ; Make a call to the memory location where the Kernel was loaded...
+    MOV RAX,  QWORD 0x100000
+    CALL RAX
