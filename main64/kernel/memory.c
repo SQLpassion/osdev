17200e3ab67334498dd17a1abd017688dcbe3571
Klaus Aschenbrenner <klaus.aschenbrenner@sqlpassion.at>
2023-01-24 23:19:21 +0100

    Started the work on the Physical Memory Manager.

--- main64/kernel/memory.c
+++ main64/kernel/memory.c
@@ -12,21 +12,111 @@ char *MemoryRegionType[] =
     "ACPI NVS Memory"
 };
 
-// Initializes the physical Memory Manager
+// Memory Map 4 GB - VMware Fusion
+// 0x00 0000 0000 - 0x00 0009 F7FF     Size: 0x00 0009 F800         638 KB              Available           653312
+// 0x00 0009 F800 - 0x00 0009 FFFF     Size: 0x00 0000 0800           2 KB              Reserved                    2048
+// 0x00 000D C000 - 0x00 000F FFFF     Size: 0x00 0002 4000         144 KB              Reserved                    147456
+// 0x00 0010 0000 - 0x00 BFED FFFF     Size: 0x00 BFDE 0000     3143552 KB = 3069 MB    Available           3218997248
+// 0x00 BFEE 0000 - 0x00 BFEF EFFF     Size: 0x00 0001 F000         124 KB              ACPI Reclaim                126976
+// 0x00 BFEF F000 - 0x00 BFEF FFFF     Size: 0x00 0000 1000           4 KB              ACPI NVS Memory             4096
+// 0x00 BFF0 0000 - 0x00 BFFF FFFF     Size: 0x00 0010 0000        1024 KB              Available           1048576
+// 0x00 F000 0000 - 0x00 F7FF FFFF     Size: 0x00 0800 0000      131072 KB = 128 MB     Reserved                    134217728
+// 0x00 FEC0 0000 - 0x00 FEC0 FFFF     Size: 0x00 0001 0000          64 KB              Reserved                    65536
+// 0x00 FEE0 0000 - 0x00 FEE0 0FFF     Size: 0x00 0000 1000           4 KB              Reserved                    4096
+// 0x00 FFFE DD00 . 0x00 FFFF FFFF     Size: 0x00 0002 0000         128 KB              Reserved                    131072
+// 0x01 0000 0000 - 0x01 3FFF FFFF     Size: 0x00 4000 0000     1048576 KB = 1024 MB    Available           1073741824
+
+// The idea of the Physical Memory Manager is to store in a Descriptor for each
+// available memory region which page frames (4K large) are available, or which ones are in use (bitmap mask).
+// With the reported Memory Map from above, we would need in sum 3 Descriptors.
+//
+// CAUTION:
+// The first free memory region is ignored, because it's below the 1MB mark (< 0x100000) -  
+// and we want to be on the safe side.
+// Only memory regions above the 1MB mark (>= 0x100000) will be available for the physical
+// Memory Manager.
+// 
+// The page frames which are already allocated to the Kernel - which is also loaded 
+// at the 1MB mark by KLDR64.BIN, are just marked as in use (bit is set to 1), when these
+// Descriptors are initialized.
+// 
+// The Descriptors needed by the Physical Memory Manager are stored in memory directly
+// after KERNEL.BIN. Therefore, we need to know the size of the loaded Kernel.
+// 
+// The following memory layout can be used to store the information of the Descriptors.
+// Number of Memory Region Descriptors
+//      => Region #1 Descriptor (24 bytes long)
+//          => Physical Start Address
+//          => Number of physical Page Frames (4096 Bytes large) available
+//              => 785888 (3218997248 / 4096)
+//          => Pointer to the physical Bitmap: PTR1
+//      => Region #2 Descriptor (24 bytes long)
+//          => Physical Start Address
+//          => Number of physical Page Frames (4096 Bytes large) available
+//              => 256 (1048576 / 4096)
+//          => Pointer to the physical Bitmap: PTR2
+//      => Region #3 Descriptor (24 bytes long)
+//          => Physical Start Address
+//          => Number of physical Page Frames (4096 Bytes large) available
+//              => 262144 (1073741824 / 4096)
+//          => Pointer to the physical Bitmap: PTR3
+// Physical Bitmap for Descriptor #1: PTR1
+//      => Size depends on the number of Page Frames managed by this Descriptor
+//      => 785888 / 8 bits/byte = 98236 bytes long
+// Physical Bitmap for Descriptor #2: PTR2
+//      => Size depends on the number of Page Frames managed by this Descriptor
+//      => 256 / 8 bits/byte = 32 bytes long
+// Physical Bitmap for Descriptor #3: PTR3
+//      => Size depends on the number of Page Frames managed by this Descriptor
+//      => 262144 / 8 bits/byte = 32768 bytes long
 void InitMemoryManager()
 {
     BiosInformationBlock *bib = (BiosInformationBlock *)BIB_OFFSET;
     MemoryRegion *region = (MemoryRegion *)MEMORYMAP_OFFSET;
+    char str[32] = "";
     int i;
 
-    // Loop over each entry
+    // The PhysicalMemoryLayout structure will be physically placed directly after the
+    // file KERNEL.BIN in memory.
+    unsigned long startAddress = 0x2000;
+    PhysicalMemoryLayout *memLayout = (PhysicalMemoryLayout *)startAddress;
+    memLayout->MemoryRegionCount = 0;
+
+    // Loop over each Memory Map entry that we got from the BIOS
     for (i = 0; i < bib->MemoryMapEntries; i++)
     {
+        // Check, if we deal with a free memory region
         if (region[i].Type == 1)
         {
             // Available
             bib->AvailableMemory += region[i].Size;
+
+            // To be on the safe side, we ignore all memory regions below the 1MB mark...
+            if (region[i].Start >= 0x100000)
+            {
+                // Create a new MemoryRegionDescriptor.
+                // Its memory address is +8 bytes after the start address of the PhysicalMemoryLayout structure.
+                MemoryRegionDescriptor *descriptor = (MemoryRegionDescriptor *)((memLayout->MemoryRegionCount * sizeof(MemoryRegionDescriptor)) + startAddress + 8);
+                descriptor->PhysicalMemoryStartAddress = region[i].Start;
+                descriptor->AvailablePageFrames = region[i].Size / 4096;
+
+                // Store the MemoryRegionDescriptor in the array
+                memLayout->Regions[memLayout->MemoryRegionCount] = *descriptor;
+
+                // Increment the Memory Region count
+                memLayout->MemoryRegionCount++;
+            }
         }
+    }
+
+    for (i = 0; i < memLayout->MemoryRegionCount; i++)
+    {
+        ltoa(memLayout->Regions[i].PhysicalMemoryStartAddress, 16, str);
+        printf(str);
+        printf("   ");
+        ltoa(memLayout->Regions[i].AvailablePageFrames, 10, str);
+        printf(str);
+        printf("\n");
     }
 }
 
@@ -108,62 +198,3 @@ void PrintMemoryMap()
     printf(str);
     printf(" MB");
 }
-
-// Memory Map 4 GB - VMware Fusion
-// 0x00 0000 0000 - 0x00 0009 F7FF     Size: 0x00 0009 F800         638 KB              Available           653312
-// 0x00 0009 F800 - 0x00 0009 FFFF     Size: 0x00 0000 0800           2 KB              Reserved                    2048
-// 0x00 000D C000 - 0x00 000F FFFF     Size: 0x00 0002 4000         144 KB              Reserved                    147456
-// 0x00 0010 0000 - 0x00 BFED FFFF     Size: 0x00 BFDE 0000     3143552 KB = 3069 MB    Available           3218997248
-// 0x00 BFEE 0000 - 0x00 BFEF EFFF     Size: 0x00 0001 F000         124 KB              ACPI Reclaim                126976
-// 0x00 BFEF F000 - 0x00 BFEF FFFF     Size: 0x00 0000 1000           4 KB              ACPI NVS Memory             4096
-// 0x00 BFF0 0000 - 0x00 BFFF FFFF     Size: 0x00 0010 0000        1024 KB              Available           1048576
-// 0x00 F000 0000 - 0x00 F7FF FFFF     Size: 0x00 0800 0000      131072 KB = 128 MB     Reserved                    134217728
-// 0x00 FEC0 0000 - 0x00 FEC0 FFFF     Size: 0x00 0001 0000          64 KB              Reserved                    65536
-// 0x00 FEE0 0000 - 0x00 FEE0 0FFF     Size: 0x00 0000 1000           4 KB              Reserved                    4096
-// 0x00 FFFE DD00 . 0x00 FFFF FFFF     Size: 0x00 0002 0000         128 KB              Reserved                    131072
-// 0x01 0000 0000 - 0x01 3FFF FFFF     Size: 0x00 4000 0000     1048576 KB = 1024 MB    Available           1073741824
-
-
-// The idea of the Physical Memory Manager is to store in a Descriptor for each
-// available memory region which page frames (4K large) are available, or in use (bitmap mask).
-// With the reported Memory Map from above, we would need in sum 3 Descriptors.
-//
-// CAUTION:
-// The first free memory region is ignored, because it's below the 1MB mark (< 0x100000) -  
-// and we want to be on the safe side.
-// Only memory regions above the 1MB mark (>= 0x100000) will be available for the physical
-// Memory Manager.
-// 
-// The page frames which are already allocated to the Kernel - which is also loaded 
-// at the 1MB mark by KLDR64.BIN, are just marked as in use (bit is set to 1), when these
-// Descriptors are initialized.
-// 
-// The Descriptors needed by the Physical Memory Manager are stored in memory directly
-// after KERNEL.BIN. Therefore, we need to know the size of the loaded Kernel.
-// 
-// The following memory layout can be used to store the information of the Descriptors.
-// Number of Memory Region Descriptors
-//      => Region #1 Descriptor (32 bytes long)
-//          => Physical Start Address
-//          => Number of physical Page Frames available (4096 Bytes large)
-//              => 785888 (3218997248 / 4096)
-//          => Pointer to the physical Bitmap: PTR1
-//      => Region #2 Descriptor (32 bytes long)
-//          => Physical Start Address
-//          => Number of physical Page Frames available (4096 Bytes large)
-//              => 256 (1048576 / 4096)
-//          => Pointer to the physical Bitmap: PTR2
-//      => Region #3 Descriptor (32 bytes long)
-//          => Physical Start Address
-//          => Number of physical Page Frames available (4096 Bytes large)
-//              => 262144 (1073741824 / 4096)
-//          => Pointer to the physical Bitmap: PTR3
-// Physical Bitmap for Descriptor #1: PTR1
-//      => Size depends on the number of Page Frames managed by this Descriptor
-//      => 785888 / 8 bits/byte = 98236 bytes long
-// Physical Bitmap for Descriptor #2: PTR2
-//      => Size depends on the number of Page Frames managed by this Descriptor
-//      => 256 / 8 bits/byte = 32 bytes long
-// Physical Bitmap for Descriptor #3: PTR3
-//      => Size depends on the number of Page Frames managed by this Descriptor
-//      => 262144 / 8 bits/byte = 32768 bytes long
--- main64/kernel/memory.h
+++ main64/kernel/memory.h
@@ -14,6 +14,33 @@ typedef struct MemoryRegion
     int	Reserved;			// Reserved
 } MemoryRegion;
 
+// Describes a single Memory Region that is managed by the
+// Physical Memory Manager.
+typedef struct MemoryRegionDescriptor
+{
+    unsigned long PhysicalMemoryStartAddress;
+    unsigned long AvailablePageFrames;
+    unsigned long BitmapMaskStartAddress;
+} MemoryRegionDescriptor;
+
+// Describes the whole memory layout that is managed by the
+// Physical Memory Manager.
+typedef struct PhysicalMemoryLayout
+{
+    // The number of managed memory regions.
+    unsigned int MemoryRegionCount;
+
+    // We have to make sure that we pad all previous structure members to multiple
+    // of 8 bytes, otherwise we get an unaligned pointer for the following array.
+    // Therefore, we add here 4 additional bytes, so that the array "Regions" starts
+    // at a multiple of 8 bytes.
+    unsigned int padding;
+
+    // The MemoryRegionDescriptor array is at the end of this struct, because
+    // it has a dynamic size based on the number of memory regions.
+    MemoryRegionDescriptor Regions[];
+} PhysicalMemoryLayout;
+
 // Initializes the physical Memory Manager
 void InitMemoryManager();
 
